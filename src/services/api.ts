1cd979dc29f28001f030591c57002b9b0e4cedb1
Lucas <lucas.teixeira@calindra.com.br>
2021-04-24 13:48:52 -0300

    API online e package

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: 'http://192.168.1.65:8080'
+    // baseURL: 'https://my-json-server.typicode.com/lucassdr/plantmanager-api'
 })
 
 export default api
